8bb5179fb80cacb99cd0fe0b10dc2c8cea3b35be
Nirex0 <Nirex.0@gmail.com>
2019-03-31 01:24:55 +0430

    Create TCPBroadcastArgs.cs

---
+++ src/NConnectivity/Dispatch/TCP/EventArguments/TCPBroadcastArgs.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Sockets;
+
+namespace NConnectivity.Dispatch.TCP.EventArguments
+{
+    class TCPBroadcastArgs : TCPArgs
+    {
+        public TCPBroadcastArgs(Socket sock, byte[] data, int size) : base(sock)
+        {
+            Data = data;
+            SentSize = size;
+        }
+
+        public byte[] Data { get; protected set; }
+        public int SentSize { get; protected set; }
+    }
+}
