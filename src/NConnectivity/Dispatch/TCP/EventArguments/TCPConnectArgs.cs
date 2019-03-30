fd0b0e6525f2d0d0105cd7d1aeb2d2634aaf0f1a
Nirex0 <Nirex.0@gmail.com>
2019-03-31 01:51:53 +0430

    Create TCPConnectArgs.cs

---
+++ src/NConnectivity/Dispatch/TCP/EventArguments/TCPConnectArgs.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace NConnectivity.Dispatch.TCP.EventArguments
+{
+    public class TCPConnectArgs : TCPArgs
+    {
+        public TCPConnectArgs(Socket sock) : base(sock)
+        {
+        }
+    }
+}
