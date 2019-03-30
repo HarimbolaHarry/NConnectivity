3292834792b8e73409ba946f70b728aea83a517a
Nirex0 <Nirex.0@gmail.com>
2019-03-31 01:25:02 +0430

    Create TCPReceiveArgs.cs

---
+++ src/NConnectivity/Dispatch/TCP/EventArguments/TCPReceiveArgs.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+
+namespace NConnectivity.Dispatch.TCP.EventArguments
+{
+    public class TCPReceiveArgs : TCPArgs
+    {
+        public TCPReceiveArgs(Socket sock, byte[] data, int size) : base(sock)
+        {
+            Data = data;
+            ReceivedSize = size;
+        }
+
+        public byte[] Data { get; protected set; }
+
+        public int ReceivedSize { get; protected set; }
+    }
+}
