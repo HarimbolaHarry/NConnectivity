797fb979c4a0797ed8b0ad651d9da43d3ce3e53f
Nirex0 <Nirex.0@gmail.com>
2019-03-31 01:24:50 +0430

    Create TCPArgs.cs

---
+++ src/NConnectivity/Dispatch/TCP/EventArguments/TCPArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace NConnectivity.Dispatch.TCP.EventArguments
+{
+    public class TCPArgs : EventArgs
+    {
+        public TCPArgs(Socket sock)
+        {
+            Connection = sock;
+        }
+
+        public Socket Connection { get; protected set; }
+    }
+}
