815aa5da35d9a02d22fbefe135a24b8fc5076332
Nirex0 <Nirex.0@gmail.com>
2019-03-31 01:25:07 +0430

    Create TCPSendArgs.cs

---
+++ src/NConnectivity/Dispatch/TCP/EventArguments/TCPSendArgs.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Sockets;
+
+namespace NConnectivity.Dispatch.TCP.EventArguments
+{
+    public class TCPSendArgs : TCPArgs
+    {
+        public TCPSendArgs(Socket sock, byte[] data, int size) : base(sock)
+        {
+            Data = data;
+            SentSize = size;
+        }
+
+        public byte[] Data { get; protected set; }
+        public int SentSize { get; protected set; }
+    }
+}
