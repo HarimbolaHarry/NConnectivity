28a45d369e09e0a6fc4c847da4b36a8937063bff
Nirex0 <Nirex.0@gmail.com>
2019-03-31 01:25:04 +0430

    Create TCPDisconnectArgs.cs

---
+++ src/NConnectivity/Dispatch/TCP/EventArguments/TCPDisconnectArgs.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace NConnectivity.Dispatch.TCP.EventArguments
+{
+    public class TCPDisconnectArgs : TCPArgs
+    {
+        public TCPDisconnectArgs(Socket sock) : base(sock)
+        {
+        }
+    }
+}
