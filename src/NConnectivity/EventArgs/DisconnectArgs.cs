3223c685c661d7fd9b0af7cc119006f515bbfb2c
Nirex0 <Nirex.0@gmail.com>
2019-03-31 05:46:48 +0430

    Rename: TCPDisonnectArgs to DisonnectArgs

---
+++ src/NConnectivity/EventArgs/DisconnectArgs.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace NConnectivity.EventArgs
+{
+    public class DisconnectArgs : SocketArgs
+    {
+        public DisconnectArgs(Socket sock) : base(sock)
+        {
+        }
+    }
+}
