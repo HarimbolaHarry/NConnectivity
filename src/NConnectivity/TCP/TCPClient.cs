f8aa814e51736cdbfedd66ee5a3f64f62dd5a93a
Nirex0 <Nirex.0@gmail.com>
2019-03-31 05:49:44 +0430

    Move: TCPClient is no longer in Core folder

--- src/NConnectivity/TCP/TCPClient.cs
+++ src/NConnectivity/TCP/TCPClient.cs
@@ -3,7 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using NConnectivity.EventArgs;
 
-namespace NConnectivity.TCP.Core
+namespace NConnectivity.TCP
 {
     /// <summary>
     /// Async TCP Client Socket class Wrapper.
