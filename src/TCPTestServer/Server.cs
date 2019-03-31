3fd52f4518892f41257c4f552b32e57712e01115
Nirex0 <Nirex.0@gmail.com>
2019-03-31 07:57:37 +0430

    Create: Server example

---
+++ src/TCPTestServer/Server.cs
@@ -0,0 +1,32 @@
+﻿// © 2019 NIREX ALL RIGHTS RESERVED
+
+using System;
+using System.Text;
+using NConnectivity.TCP;
+
+namespace TCPTestServer
+{
+    class Server
+    {
+        static TCPServer server;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+
+            server = new TCPServer("127.0.0.1", 42422, 1024);
+            server.Accept += Server_Accept;
+
+            while (true) { }
+        }
+
+        private static void Server_Accept(object sender, NConnectivity.EventArgs.SocketArgs e)
+        {
+            Console.WriteLine("Client Connected!");
+            byte[] x = Encoding.ASCII.GetBytes("Testing!\n");
+            server.BeginSend(e.Connection, x);
+            server.BeginSend(e.Connection, x);
+            server.BeginSend(e.Connection, x);
+        }
+    }
+}
