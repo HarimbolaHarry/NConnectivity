7e560eb17dcd21741d96aaf6de77c97e67406113
Nirex0 <Nirex.0@gmail.com>
2019-03-31 07:57:28 +0430

    Create: Client example

---
+++ src/TCPTestClient/Client.cs
@@ -0,0 +1,30 @@
+﻿// © 2019 NIREX ALL RIGHTS RESERVED
+
+using System;
+using System.Text;
+using NConnectivity.TCP;
+
+namespace TCPTestClient
+{
+    class Client
+    {
+        static TCPClient client;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            client = new TCPClient("127.0.0.1", 42422);
+            client.Receive += Client_Receive;
+
+            while (true) { }
+        }
+
+        private static void Client_Receive(object sender, NConnectivity.EventArgs.SocketArgs e)
+        {
+            NConnectivity.EventArgs.ReceiveArgs args = (NConnectivity.EventArgs.ReceiveArgs)e;
+            string x = Encoding.ASCII.GetString(args.Get());
+            Console.Write(x);
+        }
+    }
+}
