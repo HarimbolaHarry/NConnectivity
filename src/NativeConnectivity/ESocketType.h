c09ba7ac70f60c080463049f60c428d0a0fbe268
Nirex0 <Nirex.0@gmail.com>
2019-04-02 02:32:02 +0430

    Create ESocketType.h

---
+++ src/NativeConnectivity/ESocketType.h
@@ -0,0 +1,19 @@
+// © 2019 NIREX ALL RIGHTS RESERVED
+
+#ifndef _E_SOCKET_TYPE_H_
+#define _E_SOCKET_TYPE_H_
+
+namespace NConnectivity
+{
+	enum class ESocketType
+	{
+		ST_SREAM = 1,
+		ST_DGRAM = 2,
+		ST_RAW = 3,
+		ST_RDM = 4,
+		ST_SEQPACKET = 5
+
+	};
+}
+
+#endif // !_E_SOCKET_TYPE_H_
