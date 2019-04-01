2a42fef3192fcf52f60a530340f47386c609963d
Nirex0 <Nirex.0@gmail.com>
2019-04-02 01:33:07 +0430

    Create ISendable.h

---
+++ src/NativeConnectivity/ISendable.h
@@ -0,0 +1,13 @@
+// © 2019 NIREX ALL RIGHTS RESERVED
+
+#ifndef _I_SENDABLE_H_
+#define _I_SENDABLE_H_
+
+#include "NDefines.h"
+
+namespace NConnectivity
+{
+	class NAPI ISendable {};
+}
+
+#endif // !_I_SENDABLE_H_
