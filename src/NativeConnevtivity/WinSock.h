d3b4bc4ee13c1bc61f6534d446486944e2c2f537
Nirex0 <Nirex.0@gmail.com>
2019-04-01 18:31:26 +0430

    Create WinSock.h

---
+++ src/NativeConnevtivity/WinSock.h
@@ -0,0 +1,15 @@
+// © 2019 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_WINSOCK_H_
+#define _N_WINSOCK_H_
+
+#pragma comment(lib, "ws2_32.lib")
+
+#include "NWindows.h"
+
+#define _INC_WINDOWS
+#include <WinSock2.h>
+#undef _INC_WINDOWS
+
+
+#endif // !_N_WINSOCK_H_
