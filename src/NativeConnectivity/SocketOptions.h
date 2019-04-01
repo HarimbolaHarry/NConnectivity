8f864159f8b8135c07daf0559082b62c83732372
Nirex0 <Nirex.0@gmail.com>
2019-04-02 02:20:57 +0430

    Create: SocketOptions Struct

---
+++ src/NativeConnectivity/SocketOptions.h
@@ -0,0 +1,16 @@
+// © 2019 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_SOCKET_OPTIONS_H_
+#define _N_SOCKET_OPTIONS_H_
+
+#include <string>
+
+typedef struct
+{
+	int socket_type;
+	int socket_protocol; 
+	int socket_flags;
+
+} SocketOptions;
+
+#endif // !_N_SOCKET_OPTIONS_H_
