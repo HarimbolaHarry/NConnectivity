59b43d7391d97a7b01c9722de2a646ef0ccd2203
Nirex0 <Nirex.0@gmail.com>
2019-04-02 01:33:02 +0430

    Create: TCPServer Socket wrapper class

---
+++ src/NativeConnectivity/TCPServer.cpp
@@ -0,0 +1,13 @@
+// © 2019 NIREX ALL RIGHTS RESERVED
+
+#include "TCPServer.h"
+
+
+TCPServer::TCPServer()
+{
+}
+
+
+TCPServer::~TCPServer()
+{
+}
---
+++ src/NativeConnectivity/TCPServer.h
@@ -0,0 +1,13 @@
+// © 2019 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_TCP_SERVER_H_
+#define _N_TCP_SERVER_H_
+
+class TCPServer
+{
+public:
+	TCPServer();
+	~TCPServer();
+};
+
+#endif // !_N_TCP_SERVER_H_
