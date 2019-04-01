3ac26cd1e862749f93b3524b913b9966d102ce02
Nirex0 <Nirex.0@gmail.com>
2019-04-02 02:20:50 +0430

    Create: IPEndPoint Struct

---
+++ src/NativeConnectivity/IPEndPoint.h
@@ -0,0 +1,15 @@
+// © 2019 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_IP_ENDPOINT_H_
+#define _N_IP_ENDPOINT_H_
+
+#include <string>
+
+typedef struct 
+{
+	std::string ip;
+	int port;
+
+} IPEndPoint;
+
+#endif // !_N_IP_ENDPOINT_H_
