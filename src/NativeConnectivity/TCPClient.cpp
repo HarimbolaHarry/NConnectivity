28c474e0c367ebf66c1ae865642b4b8e749f01c5
Nirex0 <Nirex.0@gmail.com>
2019-04-02 01:32:55 +0430

    Create: TCPClient Socket wrapper class

---
+++ src/NativeConnectivity/TCPClient.cpp
@@ -0,0 +1,14 @@
+// © 2019 NIREX ALL RIGHTS RESERVED
+
+#include "TCPClient.h"
+#include "WinSock.h"
+
+TCPClient::TCPClient()
+{
+	
+}
+
+
+TCPClient::~TCPClient()
+{
+}
---
+++ src/NativeConnectivity/TCPClient.h
@@ -0,0 +1,13 @@
+// © 2019 NIREX ALL RIGHTS RESERVED
+
+#ifndef _N_TCP_CLIENT_H_
+#define _N_TCP_CLIENT_H_
+
+class TCPClient
+{
+public:
+	TCPClient();
+	~TCPClient();
+};
+
+#endif // !_N_TCP_CLIENT_H_
